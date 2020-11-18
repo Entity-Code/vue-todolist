ae721f5cd6082ce8483e334d61e26022c3cd3122
Entity-Code <mattia.tummolilllo@gmail.com>
2020-11-18 15:59:42 +0100

    Completamento esercizio

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 // todo list
 var container = new Vue({
-   el: "#container",
+   el: "#app",
 
    data: {
-      msg: ""
+      show: true,
+      items: ["Cosa da fare n.1", "Cosa da fare n.2", "Cosa da fare n.3"],
+      newItem: "",
    },
 
-   // methods: {
-   //
-   // }
+   methods: {
+      addItem: function() {
+         if (this.newItem == "") {
+            alert("Da come vedo non hai nulla da fare!")
+         } else {
+            this.items.push(this.newItem);
+         }
+      },
+
+      removeItem: function(index) {
+         this.items.splice(index, 1);
+      }
+   }
 });
