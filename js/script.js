5151fc025d5e91d8cd9cb083011e7e7443cb8387
Entity-Code <mattia.tummolilllo@gmail.com>
2020-11-18 16:02:23 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ var container = new Vue({
    el: "#app",
 
    data: {
-      show: true,
       items: ["Cosa da fare n.1", "Cosa da fare n.2", "Cosa da fare n.3"],
-      newItem: "",
+      newItem: ""
    },
 
    methods: {
+      // aggiunta on click btn
       addItem: function() {
          if (this.newItem == "") {
             alert("Da come vedo non hai nulla da fare!")
@@ -17,6 +17,7 @@ var container = new Vue({
          }
       },
 
+      // rimozione on click x
       removeItem: function(index) {
          this.items.splice(index, 1);
       }
